9fd95817001a441560dbc301d85878c616811598
Yann Mazita <dev.mazita@gmail.com>
2024-06-06 19:37:07 +0200

    AuthService update

--- app/src/main/java/com/example/create_web_app/auth/service/AuthService.java
+++ app/src/main/java/com/example/create_web_app/auth/service/AuthService.java
@@ -1,30 +1,32 @@
 package com.example.create_web_app.auth.service;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
 import com.example.create_web_app.auth.dto.AuthDto;
 import com.example.create_web_app.auth.model.Token;
+import com.example.create_web_app.auth.model.TokenData;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.MacAlgorithm;
-
-import java.nio.charset.StandardCharsets;
-import java.util.Date;
-import java.util.Map;
-
-import javax.crypto.SecretKey;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.Password;
 
 public class AuthService {
 
-    private String createAccessToken(Map<String, Object> data, long expiresDelta) {
+    private String createAccessToken(TokenData data, long expiresDelta) {
 
-        long nowTime = System.currentTimeMillis();
-        Date now = new Date(nowTime);
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime expire = now.plus(Duration.ofMillis(expiresDelta));
 
         Password key = Keys.password(System.getenv("SECRET_KEY").toCharArray());
 
         // Depending on key length, either HS256, HS384 or HS512 will be used
         String jws = Jwts.builder()
-                .subject(data.get("sub").toString())
-                .scopes(data.get("scopes").toString())
+                .subject(data.getUsername())
+                .expiration(Date.from(expire.atZone(ZoneId.systemDefault()).toInstant()))
+                .claim("scopes", data.getScopes())
                 .signWith(key)
                 .compact();
         return jws;
@@ -32,6 +34,8 @@ public class AuthService {
 
     public Token loginForAccessToken(AuthDto authDto) {
         Token token = new Token();
+        long accessTokenExpires = Long.parseLong(System.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"));
+        String access_token = createAccessToken(null, accessTokenExpires);
         return token;
     }
 

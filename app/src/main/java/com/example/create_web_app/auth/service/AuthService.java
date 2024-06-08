11512b670ca31da91a91e894a08ba90795a03b66
Yann Mazita <dev.mazita@gmail.com>
2024-06-08 16:47:49 +0200

    Auth update

--- app/src/main/java/com/example/create_web_app/auth/service/AuthService.java
+++ app/src/main/java/com/example/create_web_app/auth/service/AuthService.java
@@ -1,46 +1,65 @@
 package com.example.create_web_app.auth.service;
 
+import java.security.Key;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.stereotype.Component;
 
 import com.example.create_web_app.auth.dto.AuthDto;
 import com.example.create_web_app.auth.model.Token;
 import com.example.create_web_app.auth.model.TokenData;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.Password;
 
+@Component
 public class AuthService {
 
-    private String createAccessToken(TokenData data, long expiresDelta) {
+    Password key = Keys.password(System.getenv("SECRET_KEY").toCharArray());
 
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime expire = now.plus(Duration.ofMillis(expiresDelta));
+    private String createToken(Map<String, Object> claims, String username) {
 
-        Password key = Keys.password(System.getenv("SECRET_KEY").toCharArray());
+        long expire = Long.parseLong(System.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")) * 1000 * 60;
+        Date access_token_expires = new Date(System.currentTimeMillis() + expire);
 
-        // Depending on key length, either HS256, HS384 or HS512 will be used
-        String jws = Jwts.builder()
-                .subject(data.getUsername())
-                .expiration(Date.from(expire.atZone(ZoneId.systemDefault()).toInstant()))
-                .claim("scopes", data.getScopes())
-                .signWith(key)
+        return Jwts.builder()
+                .claims(claims)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(access_token_expires)
+                .signWith(this.key)
                 .compact();
-        return jws;
     }
 
-    public Token loginForAccessToken(AuthDto authDto) {
-        Token token = new Token();
-        long accessTokenExpires = Long.parseLong(System.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"));
-        String access_token = createAccessToken(null, accessTokenExpires);
-        return token;
+    private Jws<Claims> extractAllClaims(String token) {
+        try {
+            return Jwts
+                    .parser()
+                    .verifyWith(this.key)
+                    .build()
+                    .parseSignedClaims(token);
+        } catch (JwtException exception) {
+            throw new JwtException("Invalid Token");
+        }
     }
 
-    public Token registerForAccessToken(AuthDto authDto) {
-        Token token = new Token();
-        return token;
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public String GenerateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, username);
     }
 }

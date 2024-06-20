62d083151c933c0c57cdc341396da3f4558a22e8
Yann Mazita <dev.mazita@gmail.com>
2024-06-20 19:07:51 +0200

    Updated authentication DTO

--- app/src/main/java/com/example/create_web_app/auth/dto/AuthDto.java
+++ app/src/main/java/com/example/create_web_app/auth/dto/AuthDto.java
@@ -1,14 +1,10 @@
 package com.example.create_web_app.auth.dto;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class AuthDto {
 
-    @NotNull
     private String username;
-
-    @NotNull
     private String password;
 }

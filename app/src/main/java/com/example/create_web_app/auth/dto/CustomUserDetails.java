33102bdf5c1b45150bcc00dc9071ff4103cae8cb
Yann Mazita <dev.mazita@gmail.com>
2024-06-23 11:40:33 +0200

    Added overrides

--- app/src/main/java/com/example/create_web_app/auth/dto/CustomUserDetails.java
+++ app/src/main/java/com/example/create_web_app/auth/dto/CustomUserDetails.java
@@ -61,4 +61,24 @@ public class CustomUserDetails implements UserDetails {
     public String getUsername() {
         return this.user.getUsername();
     }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }

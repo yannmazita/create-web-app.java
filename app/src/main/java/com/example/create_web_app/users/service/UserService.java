2ff98879cecb6f9b580f6998d674042b5c9472ed
Yann Mazita <dev.mazita@gmail.com>
2024-07-06 16:55:05 +0200

    Implemented user username, password and role updating methods

--- app/src/main/java/com/example/create_web_app/users/service/UserService.java
+++ app/src/main/java/com/example/create_web_app/users/service/UserService.java
@@ -1,12 +1,14 @@
 
 package com.example.create_web_app.users.service;
 
+import com.example.create_web_app.users.dto.UserUpdate;
 import com.example.create_web_app.users.model.User;
 import com.example.create_web_app.users.repo.UserRepository;
 
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,25 +18,68 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public User createUser(User user) {
-        return userRepository.save(user);
+    /**
+     * Updates a user's username.
+     *
+     * Dedicated method for username updates. If other data besides username is
+     * present, it is ignored.
+     * 
+     * @param id   the user's ID.
+     * @param data the new username.
+     * @return the updated user.
+     */
+    public User updateUserUsername(UUID id, UserUpdate data) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user != null) {
+            user.setUsername(data.getUsername().toString());
+            return userRepository.save(user);
+        }
+        return null;
     }
 
-    public User getUser(UUID id) {
-        return userRepository.findById(id).orElse(null);
+    /**
+     * Updates a user's password.
+     *
+     * Dedicated method for password updates. If other data besides password
+     * information is present, it is ignored.
+     * 
+     * @param id   the user's ID.
+     * @param data the new password.
+     * @return the updated user.
+     */
+    public User updateUserPassword(UUID id, UserUpdate data) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user != null) {
+            user.setHashedPassword(passwordEncoder.encode(data.getNewPassword().toString()));
+            return userRepository.save(user);
+        }
+        return null;
     }
 
-    public User getUserByUsername(String username) {
-        return userRepository.findByUsername(username);
-    }
-
-    public void deleteUser(UUID id) {
-        userRepository.deleteById(id);
+    /**
+     * Updates a user's roles.
+     *
+     * Dedicated method for role updates. If other data besides roles is present, it
+     * is ignored.
+     * 
+     * @param id   the user's ID.
+     * @param data the new roles.
+     * @return the updated user.
+     */
+    public User updateUserRoles(UUID id, UserUpdate data) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user != null) {
+            user.setRoles(data.getRoles().toString());
+            return userRepository.save(user);
+        }
+        return null;
     }
 }

bde42dfeb78bdca5e8db662f65315e5d491fa687
Yann Mazita <dev.mazita@gmail.com>
2024-07-07 19:42:02 +0200

    Added startup tasks (WIP)

--- app/src/main/java/com/example/create_web_app/Application.java
+++ app/src/main/java/com/example/create_web_app/Application.java
@@ -1,13 +1,28 @@
 package com.example.create_web_app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.create_web_app.users.util.UserUtils;
 
 @SpringBootApplication
 public class Application {
+
+    @Autowired
+    private UserUtils userUtils;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public CommandLineRunner startupRunner() {
+        return (args) -> {
+            userUtils.createSuperUser();
+            userUtils.createFakeUsers();
+        };
+    }
 }
